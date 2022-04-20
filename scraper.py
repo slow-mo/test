dd760ce5046a00127d14f9899244b28019e77561
mr <mentat00@yahoo.it>
2022-04-20 11:33:09 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import os
 import scraperwiki
 
-os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
+#os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
 
 scraperwiki.sql.save(unique_keys=['id'], data={'id': 1, 'value': 100}, table_name='main')
